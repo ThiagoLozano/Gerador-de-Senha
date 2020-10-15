fc62908b4d77df675b571e368c38f38884edd817
ThiagoLozano <51161587+ThiagoLozano@users.noreply.github.com>
2020-10-15 15:05:06 -0300

    SQL

--- login_python.sql
+++ login_python.sql
@@ -1,14 +1,20 @@
-DROP DATABASE IF EXISTS login_python;
-CREATE DATABASE login_python;
+-- Cria o Banco de Dados.
+CREATE DATABASE IF NOT EXISTS login_python;
 USE login_python;
 
-CREATE TABLE IF NOT EXISTS usuario(
-`id_login` INT NOT NULL AUTO_INCREMENT, 
-`login` VARCHAR(10),
-`password` CHAR(8),
-PRIMARY KEY(`id_login`)
+-- Cria a Tabela 'usuarios'.
+CREATE TABLE IF NOT EXISTS usuarios(
+id INT AUTO_INCREMENT,
+nome CHAR(10) NOT NULL,
+senha CHAR(8) NOT NULL,
+PRIMARY KEY(id)
 );
 
-INSERT INTO usuario VALUES(DEFAULT, 'ADM','12345678');
+-- Insere um valor de exemplo.
+INSERT INTO usuarios VALUES(DEFAULT, 'ADM', '12345678');
 
-SELECT * FROM usuario;
+-- Retorna todos os registros da tabela 'usuarios';
+SELECT * FROM usuarios;
+
+-- Apaga todos os registros da tabela (se necessário).
+DELETE FROM usuarios;
